69570a7d5a49fbe84356dfd01bc8d6336235129a
Junaid Akbar <bc150401727@gmail.com>
2025-06-29 18:52:16 +0500

    grading system

---
+++ addition.py
@@ -0,0 +1,2 @@
+def add(a+b)
+print (5+2)
---
+++ grading.py
@@ -0,0 +1,16 @@
+import streamlit as st
+st.title('Grading system')
+marks=st.number_input("Enter obtained marks:",min_value=1)
+
+total= st.number_input("Enter total marks",min_value=1)
+
+p = marks/total * 100
+
+st.subheader(f'Your percentage: :blue[{p} %]')
+
+if p >= 80:
+    st.success('Excellent')
+elif p >= 60 and p < 80:
+    st.info('Pass')
+else:
+    st.error('Fail')
